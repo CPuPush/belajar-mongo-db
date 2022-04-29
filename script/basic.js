928536c76a058d78be64d9f200efc174d3f85682
CPuPush <fpakpahan44@gmail.com>
2022-04-29 16:20:07 +0700

    basic

---
+++ script/basic.js
@@ -0,0 +1,4 @@
+//membuat database
+use mahasiswa
+/**membuat database mahasiswa tetapi jika dilakukan pengecheckan tidak ada */
+/**database dibuat pertama kali ketika pembuatan collection pertama kali */
